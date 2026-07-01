35cd17d2f21e1c2fb34e7e1b88f386f03f127ac7
venearci10 <venearci@gmail.com>
2026-07-01 17:46:45 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'venearci-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar el Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activar y responder de manera local
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+    
